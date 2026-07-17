b0b5856a9a2af2d789a91c6ffff1ae2aace75a84
satyam <satyamchaudhary9334@gmail.com>
2026-07-17 22:56:28 +0530

    program2.java

---
+++ program2.java
@@ -0,0 +1,8 @@
+//2.Create a new Project in Intelij Idea and output “subscribe” on the console
+// Learnig java zero to advance
+// "" program !
+public class program2{
+  public static void main(String args []){
+    System.out.println("subscribe");
+  }
+}
