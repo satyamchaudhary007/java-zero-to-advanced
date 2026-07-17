423e1fa96462b38ad520eea3e66ebf74538bce14
satyam <satyamchaudhary9334@gmail.com>
2026-07-17 23:14:15 +0530

    Added program3

---
+++ program3.java
@@ -0,0 +1,27 @@
+// Learnig java zero to advance
+// * patterns:
+public class program3{
+  public static void main(String args []){
+
+    System.out.println("*");
+    System.out.println("* *");
+    System.out.println("* * *");
+    System.out.println("* * * *");
+    System.out.println("* * * * *");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("* * * * *");
+    System.out.println("* * * *");
+    System.out.println("* * *");
+    System.out.println("* *");
+    System.out.println("*");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("        *");
+    System.out.println("      * *");
+    System.out.println("    * * *");
+    System.out.println("  * * * *");
+    System.out.println("* * * * *");
+
+  }
+}
