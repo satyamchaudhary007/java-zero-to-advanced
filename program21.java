ee82b480e7c11301591ad9d1199cef222059dbd0
satyam <satyamchaudhary9334@gmail.com>
2026-07-19 15:07:26 +0530

    update program21

--- program21.java
+++ program21.java
@@ -1,11 +1,17 @@
 // Learning java zero to advanced
 // shows bitwise AND of two numbers.
-
+import java.util.Scanner;
 public class program21{
   public static void main(String[] args) {
-   int a = 1;
-   int b = 0;
-   int c = a & b ;
-   System.out.println("your bitwise result is : " + c);  
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Welcome to bitwize oparetor ");
+    System.out.print("please enter your First number : ");
+    int First = input.nextInt();
+    System.out.print("please enter your second  number : ");
+    int second = input.nextInt();
+    
+   int and  = First & second  ;
+   System.out.println("your bitwise and result is : " + and );  
   }
 }
