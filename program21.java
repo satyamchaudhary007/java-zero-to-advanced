30fff5b3f047d2d3bb4d2a9be8c454c4c12a43cd
satyam <satyamchaudhary9334@gmail.com>
2026-07-19 12:28:29 +0530

    added program21

---
+++ program21.java
@@ -0,0 +1,11 @@
+// Learning java zero to advanced
+// shows bitwise AND of two numbers.
+
+public class program21{
+  public static void main(String[] args) {
+   int a = 1;
+   int b = 0;
+   int c = a & b ;
+   System.out.println("your bitwise result is : " + c);  
+  }
+}
